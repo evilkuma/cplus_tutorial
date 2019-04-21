cc2f4ca717468b2c9bf9a8001d43e258e994e713
Arkadii Neskorod <arkadii.neskorod@gmail.com>
2019-04-21 15:50:23 +0300

    calculate +/- without brackets

--- src/002.cpp
+++ src/002.cpp
@@ -1,30 +1,90 @@
 #include <iostream>
 #include <regex>
-#include "Collection.h"
 using namespace std;
 
-StringCollection multipleReg(string in, regex reg) {
-    StringCollection sc;
-    smatch matches;
+/**
+ * Возводит число num в степень count.
+ * @param num int возводимое в степень
+ * @param count int значение степерни
+ * @return int
+ */
+int pow(int num, int count) {
+    int res = 1;
+    for(;count; count--) {
+        res *= num;
+    }
+    return res;
+}
+/**
+ * Возвращает количество чисел в цифре
+ * @param num int цифра
+ * @return int количество чисел
+ */
+int cnt_digits(int num) {
+    int i = 0;
+    for(; num > 0; i++) {
+        num /= 10;
+    }
+    return i;
+}
 
-    while (regex_search(in, matches, reg)) {
-        sc.add(matches[0]);
-        in = matches.suffix();
+// список целых чисел в формате char
+const char charInts[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+/**
+ * Преобразует символ в число.
+ * @param match char символ соответствующий числу.
+ * @return int число, либо если символ не соответствует ни одному из чисел -1.
+ */
+int charToInt(char match) {
+    for(int i = 0; i < 10; i++) {
+        if(charInts[i] == match) {
+            return  i;
+        }
+    }
+    return -1;
+};
+/**
+ * Преобразует строку в int. Возвращает первое вхожждение цифры в строку.
+ *  т.е. 10dasd => 10, dfsfd10 => 0
+ * @param match string
+ * @return int
+ */
+int strToInt(string match) {
+    int len = match.length();
+    int res_len = 0;
+    int res_vals[len];
+
+    for(;res_len < len; res_len++) {
+        int val = charToInt(match[res_len]);
+        res_vals[res_len] = val;
+        if(val == -1) break;
     }
 
-    return sc;
+    int res = 0;
+    for(int i = 0; i < res_len; i++) {
+        res += (res_vals[i] * pow(10, res_len - i - 1));
+    }
+
+    return res;
 };
 
 void run_002() {
-    const string in = "(123123+21323)+(4123)";
-    const regex reg ("\\(.*?\\)" );
+    string in = "-10-10+5-8+32";
 
-    StringCollection u = multipleReg(in, reg);
+    int res = 0;
 
-    StringItem* iter = u.getItem(0);
-    while(iter) {
-        cout << (*iter).value << endl;
-        iter = (*iter).item;
+    while(!in.empty()) {
+        bool isDiv = false;
+        int num = strToInt(in);
+        if(!num) {
+            isDiv = in[0] == '-';
+            in.erase(0, 1);
+            num = strToInt(in);
+        }
+        res += isDiv ? -1*num : num;
+        in.erase(0, cnt_digits(num));
     }
-};
 
+    // out 9
+    cout << res << endl;
+};
